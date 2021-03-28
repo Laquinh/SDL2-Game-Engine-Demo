77f2e7f7b626db56a298ed23769ae4be994b8adc
Laquin Laquih <anderaginaga2@gmail.com>
2021-03-28 16:22:05 +0200

    Start

---
+++ Project1/main.cpp
@@ -0,0 +1,15 @@
+#include "SDL.h"
+#undef main
+
+int main() {
+	SDL_Init(SDL_INIT_EVERYTHING);
+	SDL_Window* window = SDL_CreateWindow("title", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 600, 400, SDL_WINDOW_SHOWN);
+
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+	SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+	SDL_RenderClear(renderer);
+	SDL_RenderPresent(renderer);
+	SDL_Delay(3000);
+
+	return 0;
+}
