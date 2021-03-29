4a67ef80b3e7ef99791c3bd27e38190891e8b82f
Laquin Laquih <anderaginaga2@gmail.com>
2021-03-30 00:38:53 +0200

    Limit FPS

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -5,13 +5,27 @@
 #undef main
 
 int main() {
+	const int fps = 120;
+	const int frameDelay = 1000 / fps;
+
+	int frameStart;
+	int frameTime;
+
 	Game game("Title", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 600, 400, false);
 
 	while (game.is_running())
 	{
+		frameStart = SDL_GetTicks();
+
 		game.handle_events();
 		game.update();
 		game.render();
+
+		frameTime = SDL_GetTicks() - frameStart;
+		if (frameDelay > frameTime)
+		{
+			SDL_Delay(frameDelay - frameTime);
+		}
 	}
 
 	return 0;
